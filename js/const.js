5589f470a1a5de40de6287cabc46ade3498278a5
fqds <121273hvv@gmail.com>
2023-09-01 01:28:15 +0300

    initial commit

---
+++ js/const.js
@@ -0,0 +1,2 @@
+const backend_domen = "http://127.0.0.1:8080"
+export { backend_domen }
---
+++ js/login.js
@@ -0,0 +1,22 @@
+
+import { backend_domen } from "/js/const.js"
+
+function CreateSession() {
+    const login_val = document.getElementById("login").value;
+    const password_val = document.getElementById("password").value;
+    const url = backend_domen + "/api/createSession"
+    let payload = {
+        name: login_val,
+        password: password_val,
+    }
+    let post = JSON.stringify(payload)
+    let xhr = new XMLHttpRequest()
+    xhr.open('POST', url, true)
+    xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8')
+    xhr.send(post);
+    xhr.onload = function () {
+        console.log(xhr.response)
+    }
+}
+
+window.CreateSession = CreateSession
